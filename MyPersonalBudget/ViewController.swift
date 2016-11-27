fe96fb6969b4562f1a7a59d666cf910698f5aa28
Paul Jordan <paullj1@gmail.com>
2016-11-27 13:37:14 -0600

    init repo

---
+++ MyPersonalBudget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPersonalBudget
+//
+//  Created by Paul Jordan on 27/11/16.
+//  Copyright © 2016 Paul Jordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
